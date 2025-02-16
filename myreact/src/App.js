a2db5ea02754d2475322c6fc066718d5ba515117
Vikas8920 <vikasmishra0306@gmail.com>
2025-02-16 15:30:48 +0530

    16-02-2025 part-1

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,12 +1,27 @@
+import React from 'react'
+import './App.css'
 
-import './App.css';
+// Today's Topic: React JSX, Fragment tag, conditional statements, Rules related to react
 
-function App() {
+const App = () => {
+  const x = 5
+  // let text = 'Good bye'
+  // if(x<10){
+  //   text= 'Hello React'
+  // }
   return (
-    <div className="App">
-     <h1>Welcome</h1>
-    </div>
-  );
+    <>
+      <h1>React is {5*5} times better with JSX</h1>
+      <h1>React is {5*5} times better with JSX</h1>
+      <h1>React is {5*5} times better with JSX</h1>
+      <h1>React is {5*5} times better with JSX</h1>
+      <input className='input' type='text'/>
+      <br/>
+      <hr/>
+      <h1>{(x)<10?'Hello React': 'Good bye'}</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
+
